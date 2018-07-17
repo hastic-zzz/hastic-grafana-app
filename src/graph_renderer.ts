9e4b5cb0b3854b9897e113743af8096556d4d647
rozetko <rozetko@corpglory.com>
2018-07-17 18:19:27 +0300

    rm unneeded code

--- src/graph_renderer.ts
+++ src/graph_renderer.ts
@@ -26,7 +26,7 @@ import './vendor/flot/jquery.flot.events.js';
 
 // import { EventManager } from 'grafana/app/features/annotations/event_manager';
 import TimeSeries from 'grafana/app/core/time_series2';
-// import { getFlotTickDecimals } from 'grafana/app/core/utils/ticks';
+import { getFlotTickDecimals } from 'grafana/app/core/utils/ticks';
 import { tickStep } from 'grafana/app/core/utils/ticks';
 import { appEvents, coreModule } from 'grafana/app/core/core';
 import kbn from 'grafana/app/core/utils/kbn';
@@ -842,100 +842,4 @@ function updateLegendValues(data: TimeSeries[], panel) {
       series.updateLegendValues(formater, tickDecimals, scaledDecimals + 2);
     }
   }
-}
-
-function getDataMinMax(data: TimeSeries[]) {
-  let datamin = null;
-  let datamax = null;
-
-  for (let series of data) {
-    if (datamax === null || datamax < series.stats.max) {
-      datamax = series.stats.max;
-    }
-    if (datamin === null || datamin > series.stats.min) {
-      datamin = series.stats.min;
-    }
-  }
-
-  return { datamin, datamax };
-}
-
-function getFlotRange(panelMin, panelMax, datamin, datamax) {
-  const autoscaleMargin = 0.02;
-
-  let min = +(panelMin != null ? panelMin : datamin);
-  let max = +(panelMax != null ? panelMax : datamax);
-  let delta = max - min;
-
-  if (delta === 0.0) {
-    // Grafana fix: wide Y min and max using increased wideFactor
-    // when all series values are the same
-    var wideFactor = 0.25;
-    var widen = Math.abs(max === 0 ? 1 : max * wideFactor);
-
-    if (panelMin === null) {
-      min -= widen;
-    }
-    // always widen max if we couldn't widen min to ensure we
-    // don't fall into min == max which doesn't work
-    if (panelMax == null || panelMin != null) {
-      max += widen;
-    }
-  } else {
-    // consider autoscaling
-    var margin = autoscaleMargin;
-    if (margin != null) {
-      if (panelMin == null) {
-        min -= delta * margin;
-        // make sure we don't go below zero if all values
-        // are positive
-        if (min < 0 && datamin != null && datamin >= 0) {
-          min = 0;
-        }
-      }
-      if (panelMax == null) {
-        max += delta * margin;
-        if (max > 0 && datamax != null && datamax <= 0) {
-          max = 0;
-        }
-      }
-    }
-  }
-  return { min, max };
-}
-
-function getFlotTickDecimals(data, axis) {
-  let { datamin, datamax } = getDataMinMax(data);
-  let { min, max } = getFlotRange(axis.min, axis.max, datamin, datamax);
-  let noTicks = 3;
-  let tickDecimals, maxDec;
-  let delta = (max - min) / noTicks;
-  let dec = -Math.floor(Math.log(delta) / Math.LN10);
-
-  let magn = Math.pow(10, -dec);
-  // norm is between 1.0 and 10.0
-  let norm = delta / magn;
-  let size;
-
-  if (norm < 1.5) {
-    size = 1;
-  } else if (norm < 3) {
-    size = 2;
-    // special case for 2.5, requires an extra decimal
-    if (norm > 2.25 && (maxDec == null || dec + 1 <= maxDec)) {
-      size = 2.5;
-      ++dec;
-    }
-  } else if (norm < 7.5) {
-    size = 5;
-  } else {
-    size = 10;
-  }
-
-  size *= magn;
-
-  tickDecimals = Math.max(0, maxDec != null ? maxDec : dec);
-  // grafana addition
-  const scaledDecimals = tickDecimals - Math.floor(Math.log(size) / Math.LN10);
-  return { tickDecimals, scaledDecimals };
 }
