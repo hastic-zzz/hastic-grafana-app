375cc3eca5cde16d0e2aaf53cfcb8d4538e23ea8
sanke <et@corpglory.com>
2018-07-17 19:43:40 +0300

    temporary commit

--- tests/colors.jest.ts
+++ tests/colors.jest.ts
@@ -1,7 +1,7 @@
-import { ANALYTIC_UNIT_COLORS } from '../src/colors';
+import colors, { ANALYTIC_UNIT_COLORS } from '../src/colors';
+import { AnalyticController } from '../src/controllers/analytic_controller';
+import { AnalyticUnit } from '../src/models/analytic_unit';
 
-
-var text = 'hello world';
 
 var palette = [
   '#FF99FF',
@@ -13,15 +13,28 @@ var palette = [
   '#f8c171',
 ];
 
-
-describe('basic test', function(){
-  it('should return "hello world"', function() {
-    expect(text).toBe('hello world')
-  })
-});
-
 describe('test colors in the palette', function(){
   it('ANALYTIC_UNIT_COLORS should use same palette', function(){
     expect(ANALYTIC_UNIT_COLORS).toEqual(palette)
   })
+});
+
+var createArray = function() {
+  var AuArray = [];
+  var AuColors = [];
+  var i = 0;
+  while(i < 10){
+    AuArray.push(new AnalyticUnit);
+    AuColors.push(AuArray[i].color);
+    i++;
+  };
+  console.log(AuColors);
+}
+createArray();
+
+/*describe('Compare Au color to correspond to palette', function() {
+  it('should compare create an AU with the first color of the palette',function(){
+    expect(AnalyticUnit1.color).toBe(palette[0])
+  })
 })
+*/
