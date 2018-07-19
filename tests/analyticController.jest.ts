911ba038c23e4b1c5bb07906f0cb50b4c1467552
rozetko <rozetko@corpglory.com>
2018-07-19 23:59:54 +0300

    Update tests

---
+++ tests/analyticController.jest.ts
@@ -0,0 +1,32 @@
+import { ANALYTIC_UNIT_COLORS } from '../src/colors';
+import { MetricExpanded } from '../src/models/metric';
+import { DatasourceRequest } from '../src/models/datasource';
+
+import { analyticController } from './setup_tests';
+
+
+describe('AnalyticController', function () {
+  it('should create analytic units with colors from palette', async function () {
+    for (let color of ANALYTIC_UNIT_COLORS) {
+      analyticController.createNew();
+      expect(analyticController.newAnalyticUnit.color).toBe(color);
+      await analyticController.saveNew({} as MetricExpanded, {} as DatasourceRequest, 1);
+    }
+  });
+
+  it('should save analytic units', function () {
+    expect(analyticController.analyticUnits).toHaveLength(ANALYTIC_UNIT_COLORS.length);
+  });
+
+  it('should remove analytic unit with right id', async function () {
+    await analyticController.removeAnalyticUnit('2');
+    for (let analyticUnit of analyticController.analyticUnits) {
+      expect(analyticUnit.id).not.toBe('2');
+    }
+  });
+
+  it('should change color on choosing from palette', function () {
+    analyticController.onAnalyticUnitColorChange('1', 'red');
+    expect(analyticController.analyticUnits[0].color).toBe('red');
+  });
+});
