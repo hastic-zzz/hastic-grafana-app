5ca907b3c4262e3115778e391d358b3282bb13eb
rozetko <rozetko@corpglory.com>
2019-02-25 15:29:14 +0300

    Hastic URL doesn't update #172 (#189)

--- src/config/config_ctrl.ts
+++ src/config/config_ctrl.ts
@@ -1,13 +1,36 @@
 import template from './template.html';
-
+import { normalizeUrl } from '../utlis';
 
 class ConfigCtrl {
   static template = template;
   appModel: any;
+  appEditCtrl: any;
+
   constructor() {
     if(this.appModel.jsonData === undefined) {
       this.appModel.jsonData = {};
     }
+
+    this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
+    this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
+  }
+
+  preUpdate() {
+    this.normalizeUrl();
+    return Promise.resolve();
+  }
+
+  postUpdate() {
+    // TODO: check whether hasticServerUrl is accessible
+    if(!this.appModel.enabled) {
+      return Promise.resolve();
+    }
+
+    return { message: 'Hastic app installed!' };
+  }
+
+  normalizeUrl() {
+    this.appModel.jsonData.hasticServerUrl = normalizeUrl(this.appModel.jsonData.hasticServerUrl);
   }
 }
 
