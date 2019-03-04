465c71ab117419b00b51616ed068a74e06d32c9a
rozetko <rozetko@corpglory.com>
2019-03-04 16:44:28 +0300

    Set default access method

---
+++ src/datasource/config_ctrl.ts
@@ -0,0 +1,32 @@
+import { normalizeUrl } from '../utlis';
+import configTemplate from './partials/config.html';
+
+
+export class HasticConfigCtrl {
+  public static template = configTemplate;
+  public ACCESS_OPTIONS = [
+    { key: 'proxy', value: 'Server (Default)' },
+    { key: 'direct', value: 'Browser' }
+  ];
+
+  private showAccessHelp: boolean = false;
+
+  constructor(private $scope: any) {
+    if(this.$scope.current === undefined) {
+      this.$scope.current = {
+        access: 'proxy'
+      };
+    }
+  }
+
+  normalizeUrl() {
+    if(this.$scope.current.url === '') {
+      return;
+    }
+    this.$scope.current.url = normalizeUrl(this.$scope.current.url);
+  }
+
+  toggleAccessHelp() {
+    this.showAccessHelp = !this.showAccessHelp;
+  }
+}
--- src/datasource/module.ts
+++ src/datasource/module.ts
@@ -1,36 +1,7 @@
 import { HasticDatasource } from './datasource';
 import { HasticQueryCtrl } from './query_ctrl';
+import { HasticConfigCtrl } from './config_ctrl';
 
-import { normalizeUrl } from '../utlis';
-
-import configTemplate from './partials/config.html';
-
-
-class HasticConfigCtrl {
-  public static template = configTemplate;
-  public ACCESS_OPTIONS = [
-    { key: 'proxy', value: 'Server (Default)' },
-    { key: 'direct', value: 'Browser'}
-  ];
-
-  public current: any;
-  private showAccessHelp: boolean = false;
-
-  constructor(private $scope) {
-  }
-
-  normalizeUrl() {
-    if(this.$scope.current.url === "") {
-      return;
-    }
-    this.$scope.current.url = normalizeUrl(this.$scope.current.url);
-  }
-
-  toggleAccessHelp() {
-    this.showAccessHelp = !this.showAccessHelp;
-  }
-
-}
 
 export {
   HasticDatasource as Datasource,
