ede504bec345fd7745439ff46c8ba87ab1209abf
rozetko <rozetko@corpglory.com>
2019-03-04 20:56:03 +0300

    Finalyze datasource

---
+++ src/datasource/hastic_api.ts
@@ -0,0 +1,29 @@
+import { BackendSrv } from 'grafana/app/core/services/backend_srv';
+
+export default class HasticAPI {
+  private url: string;
+
+  constructor(instanceSettings: any, private backendSrv: BackendSrv) {
+    this.url = instanceSettings.url;
+  }
+
+  get(url: string, params?: any) {
+    return this._query('GET', url, params);
+  }
+
+  private async _query(method: string, url: string, data?: any) {
+    method = method.toUpperCase();
+    let options: any = {
+      method,
+      url: this.url + url
+    };
+    if(method === 'GET' || method === 'DELETE') {
+      options.params = data;
+    } else {
+      options.data = data;
+    }
+
+    const response = await this.backendSrv.datasourceRequest(options);
+    return response.data;
+  }
+}
