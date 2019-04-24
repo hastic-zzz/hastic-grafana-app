9ec52fd4ba23fd0e896b2119ebf68fa36c435ff8
rozetko <rozetko@corpglory.com>
2019-04-24 12:22:13 +0300

    Bump version to 0.3.3

--- src/utlis.ts
+++ src/utlis.ts
@@ -1,7 +1,7 @@
 import url from 'url-parse';
 import * as _ from 'lodash';
 
-export const SUPPORTED_SERVER_VERSION = '0.3.2-beta';
+export const SUPPORTED_SERVER_VERSION = '0.3.3-beta';
 
 export function normalizeUrl(inputUrl: string) {
   if(!inputUrl) {
