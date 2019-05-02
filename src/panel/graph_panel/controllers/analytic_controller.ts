004a6edfd40554e432aae7072f619d19c6bbd47a
rozetko <rozetko@hastic.io>
2019-05-02 21:13:06 +0300

    Thresholds error: from is NaN hastic/hastic-server#621 (#277)

--- src/panel/graph_panel/controllers/analytic_controller.ts
+++ src/panel/graph_panel/controllers/analytic_controller.ts
@@ -220,8 +220,8 @@ export class AnalyticController {
     this.analyticUnits.forEach(a => this._runStatusWaiter(a));
   }
 
-  fetchAnalyticUnitsDetections(from: number | null, to: number | null) {
-    if(from === null || to === null) {
+  fetchAnalyticUnitsDetections(from?: number, to?: number) {
+    if(from === undefined || to === undefined) {
       return;
     }
     this.analyticUnits.forEach(analyticUnit => {
@@ -309,7 +309,9 @@ export class AnalyticController {
 
   async redetectAll() {
     this.analyticUnits.forEach(unit => {
+      // TODO: remove duplication with runDetect
       unit.segments.clear();
+      unit.detectionSpans = [];
       unit.status = null;
     });
     const ids = this.analyticUnits.map(analyticUnit => analyticUnit.id);
@@ -318,12 +320,13 @@ export class AnalyticController {
     _.each(this.analyticUnits, analyticUnit => this._runStatusWaiter(analyticUnit));
   }
 
-  async runDetect(analyticUnitId: AnalyticUnitId) {
+  async runDetect(analyticUnitId: AnalyticUnitId, from?: number, to?: number) {
     const analyticUnit = this._analyticUnitsSet.byId(analyticUnitId);
     analyticUnit.segments.clear();
+    analyticUnit.detectionSpans = [];
     analyticUnit.status = null;
     await this.saveAnalyticUnit(analyticUnit);
-    await this._analyticService.runDetect(analyticUnitId);
+    await this._analyticService.runDetect(analyticUnitId, from, to);
     this._runStatusWaiter(analyticUnit);
   }
 
