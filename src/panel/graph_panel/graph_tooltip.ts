e3ee94172e80316f8b0faa54630994704c70ca51
VargBurz <voidstone1@mail.ru>
2019-08-27 18:45:44 +0300

    fix

--- src/panel/graph_panel/graph_tooltip.ts
+++ src/panel/graph_panel/graph_tooltip.ts
@@ -108,6 +108,7 @@ export class GraphTooltip {
         series = seriesList[hoverInfo.index];
 
         value = series.formatValue(hoverInfo.value);
+
         seriesHtml += '<div class="graph-tooltip-list-item ' + highlightClass + '"><div class="graph-tooltip-series-name">';
         seriesHtml += '<i class="fa fa-minus" style="color:' + hoverInfo.color +';"></i> ' + hoverInfo.label + ':</div>';
         seriesHtml += '<div class="graph-tooltip-value">' + value + '</div></div>';
