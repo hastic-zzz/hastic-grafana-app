08dbf0a9199b9dc8fd859b29c81b33554f2cb383
VargBurz <voidstone1@mail.ru>
2019-10-02 15:30:18 +0300

    add check for undefined

--- src/panel/graph_panel/graph_ctrl.ts
+++ src/panel/graph_panel/graph_ctrl.ts
@@ -433,9 +433,15 @@ class GraphCtrl extends MetricsPanelCtrl {
     }
 
     if(this.analyticsController === undefined || !this.analyticsController.graphLocked) {
-      this._graphRenderer.render(this.seriesList);
-      this._graphLegend.render();
-      this._graphRenderer.renderPanel();
+      if(this._graphRenderer !== undefined && this.seriesList !== undefined) {
+        this._graphRenderer.render(this.seriesList);
+      }
+      if(this._graphLegend !== undefined) {
+        this._graphLegend.render();
+      }
+      if(this._graphRenderer !== undefined) {
+        this._graphRenderer.renderPanel();
+      }
     }
   }
 
