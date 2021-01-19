f1838fc7ba10b1cd6788cd476e62855e86f93f12
rozetko <rozetko@hastic.io>
2021-01-19 15:14:52 +0300

    Bump version to 0.5.1 (#465)

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import * as _ from 'lodash';
 
 
 // TODO: move to config
-export const SUPPORTED_SERVER_VERSION = '0.5.0';
+export const SUPPORTED_SERVER_VERSION = '0.5.1';
 
 export function normalizeUrl(inputUrl: string) {
   if(!inputUrl) {
